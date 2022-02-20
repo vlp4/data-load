1273bdffbbefab3b419d64f8a38f57cfc193d9b2
leon <lv@opendev.com>
2022-02-21 02:32:18 +0400

    Tasks done

--- lesson01/task1.py
+++ lesson01/task1.py
@@ -1,2 +1,19 @@
 # 1. Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев для конкретного пользователя,
 # сохранить JSON-вывод в файле *.json.
+
+import requests
+import json
+from pprint import pprint
+
+username = 'SAMSI-RandTensors'
+
+url = f'https://api.github.com/users/{username}/repos'
+
+resp = requests.get(url)
+repos = resp.json()
+
+names = list(map(lambda repo: repo['name'], repos))
+pprint(f'Repositories loaded: {names}')
+
+with open('task1-out.json', 'w') as outfile:
+    outfile.write(json.dumps(repos, indent=4))
--- lesson01/task2.py
+++ lesson01/task2.py
@@ -2,3 +2,60 @@
 # требующее авторизацию (любого типа). Выполнить запросы к нему, пройдя авторизацию. Ответ сервера записать в файл.
 # Если нет желания заморачиваться с поиском, возьмите API вконтакте (https://vk.com/dev/first_guide). Сделайте
 # запрос, чтобы получить список всех сообществ на которые вы подписаны.
+
+import requests
+from pprint import pprint
+
+import os.path
+import json
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+def main():
+    SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('creds.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('drive', 'v3', credentials=creds)
+
+        # Call the Drive v3 API
+        results = service.files().list(pageSize=1, fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+
+        if not items:
+            print('No files found.')
+            return
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+
+        with open('task2-out.json', 'w') as outfile:
+            outfile.write(json.dumps(items, indent=4))
+
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+
+
+if __name__ == '__main__':
+    main()
